87554699072d8e7049d20c824e2a4482604925e0
mla4783 <mla4783@rit.edu>
2020-09-27 19:39:23 -0400

    Sound finished

--- Games4Change/Assets/Scripts/Audio.cs
+++ Games4Change/Assets/Scripts/Audio.cs
@@ -1,13 +1,33 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Audio : MonoBehaviour
 {
+    [SerializeField]
+    private Image volumeImage;
+    [SerializeField]
+    private Image soundImage;
+    [SerializeField]
+    private Slider volumeSlider;
+    [SerializeField]
+    private Slider soundSlider;
+    [SerializeField]
+    private Sprite[] volumeImages;
+    [SerializeField]
+    private Sprite[] soundImages;
+
+    [SerializeField]
+    private AudioSource music;
+    [SerializeField]
+    private AudioSource sfx;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        volumeSlider.onValueChanged.AddListener(delegate { ChangeVolume(); });
+        soundSlider.onValueChanged.AddListener(delegate { ChangeSound(); });
     }
 
     // Update is called once per frame
@@ -15,4 +35,51 @@ public class Audio : MonoBehaviour
     {
         
     }
+
+    public void ChangeVolume() 
+    {
+        if (volumeSlider.value == 0.0001f) 
+        {
+            volumeImage.sprite = volumeImages[3];
+        }
+        else if (volumeSlider.value > .75f)
+        {
+            volumeImage.sprite = volumeImages[0];
+        }
+        else if (volumeSlider.value > .2f)
+        {
+            volumeImage.sprite = volumeImages[1];
+        }
+        else
+        {
+            volumeImage.sprite = volumeImages[2];
+
+        }
+        music.volume = Mathf.Log10(volumeSlider.value * 20);
+    }
+
+
+    public void ChangeSound()
+    {
+        if (soundSlider.value == 0.0001f)
+        {
+            soundImage.sprite = soundImages[3];
+        }
+        else if (soundSlider.value > .75f)
+        {
+            soundImage.sprite = soundImages[0];
+        }
+        else if (soundSlider.value > .2f)
+        {
+            soundImage.sprite = soundImages[1];
+        }
+        else
+        {
+            soundImage.sprite = soundImages[2];
+
+        }
+        sfx.volume = Mathf.Log10(soundSlider.value * 20);
+    }
+
+
 }
--- Games4Change/Assets/Scripts/Groceries.cs
+++ Games4Change/Assets/Scripts/Groceries.cs
@@ -5,6 +5,10 @@ using UnityEngine;
 public class Groceries : MonoBehaviour
 {
     private bool scanned;
+    [SerializeField]
+    private AudioSource soundManager;
+    [SerializeField]
+    private AudioClip beepSound;
     // Start is called before the first frame update
     void Start()
     {
@@ -24,6 +28,7 @@ public class Groceries : MonoBehaviour
             if (!scanned)
             {
                 Debug.Log("Beep"); //replace with sound
+                soundManager.PlayOneShot(beepSound);
                 scanned = true;
             }
             else
