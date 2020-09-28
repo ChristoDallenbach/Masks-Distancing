b065e8c140fb07fdbf1c490c02bd541f6ffc8abf
ChristoDallenbach <christo.dallenbach@gmail.com>
2020-09-28 18:31:12 -0400

    Added Scipts, and grocieries work with system. They just don't go back to original position.

--- Games4Change/Assets/Scripts/Groceries.cs
+++ Games4Change/Assets/Scripts/Groceries.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Groceries : MonoBehaviour
 {
@@ -9,16 +10,37 @@ public class Groceries : MonoBehaviour
     private AudioSource soundManager;
     [SerializeField]
     private AudioClip beepSound;
+
+    // the start position of the groceries
+    private Vector3 startPosition;
+
+    // getter and setter for scanned
+    public bool Scanned
+    {
+        get { return scanned; }
+        set { scanned = value; }
+    }
+
+    public Vector3 StartPosition
+    {
+        get { return startPosition; }
+    }
+
     // Start is called before the first frame update
     void Start()
     {
-        scanned = false;
+        scanned = true;
+        gameObject.GetComponent<Image>().enabled = false;
+
     }
 
     // Update is called once per frame
     void Update()
     {
-
+        if (startPosition == null)
+        {
+            startPosition = gameObject.transform.position;
+        }
     }
     private void OnTriggerEnter2D(Collider2D other)
     {
@@ -38,10 +60,12 @@ public class Groceries : MonoBehaviour
 
         }
         //put in bag
-        if (other.name == "Bag" && scanned == true)
+        if (other.name == "Bag" && scanned == true && gameObject.GetComponent<Image>().enabled == true)
         {
-            gameObject.SetActive(false);
+            gameObject.GetComponent<Image>().enabled = false;
+            gameObject.transform.position = startPosition;
         }
 
     }
 }
+
